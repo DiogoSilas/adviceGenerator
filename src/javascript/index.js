c795d3f10a0ec68485cb019ed3928aa725d46fb9
Diogo Silas <diogosilas4@gmail.com>
2023-12-31 12:33:35 -0300

    first commit

---
+++ src/javascript/index.js
@@ -0,0 +1,22 @@
+const btnAdvice = document.getElementById('btn-advice');
+const adviceDisplay = document.getElementById('advice-display');
+btnAdvice.addEventListener('click', async function() {
+    async function obterConselhoAleatorio() {
+        const url = 'https://api.adviceslip.com/advice';
+        const response = await fetch(url);
+        const data = await response.json();
+        return {
+            numero: data.slip.id,
+            conselho: data.slip.advice
+        };
+    }
+    try {
+        const { numero, conselho } = await obterConselhoAleatorio();
+        adviceDisplay.textContent = conselho;
+        const numeroAdvice = document.getElementById('number-advice');
+        numeroAdvice.textContent = numero;
+    } catch (error) {
+        console.error('Ocorreu um erro ao buscar o conselho:', error);
+    }
+});
+
