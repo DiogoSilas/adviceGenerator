09c63d0493c6d18477adb8779b64d3d7d9b8d113
Diogo Silas <diogosilas4@gmail.com>
2023-12-31 16:14:39 -0300

    Adicionado tratamentos de erros no código.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,20 +3,26 @@ const numeroAdvice = document.getElementById('number-advice');
 const adviceDisplay = document.getElementById('advice-display');
 btnAdvice.addEventListener('click', async function() {
     async function obterConselhoAleatorio() {
-        const url = 'https://api.adviceslip.com/advice';
-        const response = await fetch(url);
-        const data = await response.json();
-        return {
-            numero: data.slip.id,
-            conselho: data.slip.advice
-        };
+        try {
+            const url = 'https://api.adviceslip.com/advice';
+            const response = await fetch(url);
+            const data = await response.json();
+            return {
+                numero: data.slip.id,
+                conselho: data.slip.advice
+            };
+        }catch(error){
+            console.log('Ocorreu um erro ao buscar os dados da API:', error)
+        }
     }
     try {
         const { numero, conselho } = await obterConselhoAleatorio();
         adviceDisplay.textContent = `"${conselho}"`;
         numeroAdvice.textContent = numero;
-    } catch (error) {
-        console.error('Ocorreu um erro ao buscar o conselho:', error);
+    }catch(error) {
+        console.log('Ocorreu um erro ao buscar o conselho:', error);
+    }finally{
+        console.log('Após clicar no botão, é necessário esperar 5 segundos para a API mostrar outro conselho.')
     }
 });
 
